8c8bf68f8ad63d97e7ab16b124603a014af305ab
kbrumer7 <62360804+kbrumer7@users.noreply.github.com>
2020-06-04 09:41:05 -0700

    Add basics of page

---
+++ main.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const express = require("express"),
+  app = express(),
+  homeController = require("./controllers/homeController"),
+  errorController = require("./controllers/errorController"),
+  layouts = require("express-ejs-layouts");
+
+app.set("view engine", "ejs");
+app.set("port", process.env.PORT || 3000);
+app.use(
+  express.urlencoded({
+    extended: false
+  })
+);
+app.use(express.json());
+app.use(layouts);
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/kayla", (req, res) => {
+  res.render("kayla");
+});
+
+app.get("/adam", (req, res) => {
+  res.render("adam");
+});
+
+app.get("/jiale", (req, res) => {
+  res.render("jiale");
+});
+
+app.get("/joseph", (req, res) => {
+  res.render("joseph");
+});
+
+app.get("/belle", (req, res) => {
+  res.render("belle");
+});
+
+app.get("/courses", homeController.showCourses);
+app.get("/contact", homeController.showSignUp);
+app.post("/contact", homeController.postedSignUpForm);
+
+app.use(errorController.pageNotFoundError);
+app.use(errorController.internalServerError);
+
+app.listen(app.get("port"), () => {
+  console.log(`Server running at http://localhost:${app.get("port")}`);
+});
