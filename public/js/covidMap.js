9c0906d2d8aad565773af27a6041923633968239
belle scott <bscott@brandeis.edu>
2020-06-16 19:38:20 -0700

    added to map, added styling to custom

--- public/js/covidMap.js
+++ public/js/covidMap.js
@@ -1,9 +1,44 @@
 $(document).ready(function() {
   /** FIRST YOU HAVE TO INITIALIZE THE MAP **/
   $('#map').usmap({
-      stateStyles: {fill: 'blue'},
+      stateStyles: {fill: '#fff'},
         'click': function(event, data) {
-         $('#clicked-state').text('You clicked:'+data.name );
+          window.location.href='mass';
+         //$('#clicked-state').text('You clicked:'+data.name );
+    }, 
+    stateSpecificStyles: {
+      'MA': {fill: 'orange'},
+      'OR': {fill: 'orange'}
+      //'FL': {fill: 'light blue'}
     }
   });
+
+//specific state colors
+  $('#map').usmap({
+    stateSpecificStyles: {
+      'MA': {fill: 'orange'},
+      'OR': {fill: 'orange'}
+      //'FL': {fill: 'light blue'}
+    } 
+  });
+
+  $('#on-ma').click(function(event){
+    $('#map').usmap('trigger', 'MA', 'mouseover', event);
+  });
+
+  // $('#map').on('usmap<event>MA', function(event, data) {
+  //   window.location = 'mass.ejs';
+  // });
+
+  // $('#map').usmap({
+
+  //   <event>State: {
+  //     'MD' : function(event, data) {
+  //       console.log('You interacted with the state of Maryland');
+  //     }
+  //   }
+  // });
+
+  
+
   });
