b193c51042f3bd7a8d7bac41bffde1ad05bca082
jnissenbaum01 <jnissenbaum@brandeis.edu>
2020-06-18 12:49:25 -0400

    Fix capitalization problem with NY state page

--- controllers/stateController.js
+++ controllers/stateController.js
@@ -4,10 +4,11 @@ exports.showStateWithData = async (req, res, next) =>
 {
   try
   {
+      let theState = req.url.toLowerCase() //lowercase state name comes from URL, starting with "/" for examlple, "/ny" or "ca"
       let method = req.params.method
-      let result = await axios.get(`https://covidtracking.com/api/v1/states${req.url}/current.json`)//("https://covidtracking.com/api/v1/states/daily.json")
+      let result = await axios.get(`https://covidtracking.com/api/v1/states${theState}/current.json`)//("https://covidtracking.com/api/v1/states/daily.json")
       let covidData = result['data']
-      res.render(req.url.substring(1,3),{covidData:covidData})
+      res.render(theState.substring(1,3),{covidData:covidData})
 
       // if (method=="json")
       // {
