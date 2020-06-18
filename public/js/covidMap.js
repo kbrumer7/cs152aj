613e8e2dfd9e6260a2ad8cafc90322edfe96d656
belle scott <bscott@brandeis.edu>
2020-06-18 08:48:20 -0700

    clicking on state now leads to respective html page

--- public/js/covidMap.js
+++ public/js/covidMap.js
@@ -1,30 +1,189 @@
 $(document).ready(function() {
-  /** FIRST YOU HAVE TO INITIALIZE THE MAP **/
+  /** FIRST YOU HAVE TO INITIALIZE THE MAP 
+   * window.location.href= description;
+   * **/
   $('#map').usmap({
       stateStyles: {fill: '#fff'},
-        'click': function(event, data) {
-          window.location.href='ma';
-         //$('#clicked-state').text('You clicked:'+data.name );
+      'click': function(event, data) {
+          var description = "No state info loaded for this state.";
+            switch (data.name)
+            {
+              case 'AL':
+                description = 'alabama';
+                break;
+              case 'AK':
+                description = 'alaska';
+                break;
+              case 'AZ':
+                description = 'arizona';
+                break;
+              case 'AR':
+                description = 'arkansas';
+                break;
+              case 'CA':
+                description = 'ca';
+                break;
+              case 'CO':
+                description = 'colorado';
+                break;
+              case 'CT':
+                description = 'connecticut';
+                break;
+              case 'DE':
+                description = 'delaware';
+                break;
+              case 'FL':
+                description = 'florida';
+                break;
+              case 'GA':
+                description = 'georgia';
+                break;
+              case 'HI':
+                description = 'hawaii';
+                break;
+              case 'ID':
+                description = 'idaho';
+                break;
+              case 'IL':
+                description = 'illinois';
+                break;
+              case 'IN':
+                description = 'indiana';
+                break;
+              case 'IA':
+                description = 'iowa';
+                break;
+              case 'KS':
+                description = 'kansas';
+                break;
+              case 'KY':
+                description = 'kentucky';
+                break;
+              case 'LA':
+                description = 'louisiana';
+                break;
+              case 'ME':
+                description = 'maine';
+                break;
+              case 'MD':
+                description = 'maryland';
+                break;
+              case 'MA':
+                description = 'ma';
+                break;
+              case 'MI':
+                description = 'michigan';
+                break;
+              case 'MN':
+                description = 'minnesota';
+                break;
+              case 'MS':
+                description = 'mississippi';
+                break;
+              case 'MO':
+                description = 'missouri';
+                break;
+              case 'MT':
+                description = 'montana';
+                break;
+              case 'NE':
+                description = 'nebraska';
+                break;
+              case 'NV':
+                  description = 'nevada';
+                  break;
+              case 'NH':
+                  description = 'newhampshire';
+                  break;
+              case 'NJ':
+                  description = 'newjersey';
+                  break;
+              case 'NM':
+                  description = 'newmexico';
+                  break;
+              case 'NY':
+                  description = 'ny';
+                  break;
+              case 'NC':
+                  description = 'northcarolina';
+                  break;
+              case 'ND':
+                  description = 'northdakota';
+                  break;
+              case 'OH':
+                  description = 'ohio';
+                  break;
+              case 'OK':
+                  description = 'oklahoma';
+                  break;
+              case 'OR':
+                  description = 'oregon';
+                  break;
+              case 'PA':
+                  description = 'pennsylvania';
+                  break;
+              case 'RI':
+                  description = 'rhodeisland';
+                  break;
+              case 'SC':
+                  description = 'southcarolina';
+                  break;
+              case 'SD':
+                  description = 'southdakota';
+                  break;
+              case 'TN':
+                  description = 'tennessee';
+                  break;
+              case 'TX':
+                  description = 'texas';
+                  break;
+              case 'UT':
+                  description = 'utah';
+                  break;
+              case 'VT':
+                  description = 'vermont';
+                  break;
+              case 'VA':
+                  description = 'virginia';
+                  break;
+              case 'WA':
+                  description = 'washington';
+                  break;
+              case 'WV':
+                  description = 'westvirginia';
+                  break;
+              case 'WI':
+                  description = 'wisconsin';
+                  break;
+              case 'WY':
+                  description = 'wyoming';
+                  break;
+              case 'DC':
+                  description = 'washingtondc';
+                  break;
+                   // etc
+             }
+          window.location.href= description;
     },
     stateSpecificStyles: {
       'MA': {fill: 'orange'},
-      'OR': {fill: 'orange'},
+      'OR': {fill: 'red'},
       'CA': {fill: 'orange'},
       'NY': {fill: 'orange'}
       //'FL': {fill: 'light blue'}
     }
   });
 
-//specific state colors
-  $('#map').usmap({
-    stateSpecificStyles: {
-      'MA': {fill: 'orange'},
-      'OR': {fill: 'orange'},
-      'CA': {fill: 'orange'},
-      'NY': {fill: 'orange'}
-      //'FL': {fill: 'light blue'}
-    }
-  });
+// //specific state colors
+//   $('#map').usmap({
+//     stateSpecificStyles: {
+//       'MA': {fill: 'orange'},
+//       'OR': {fill: 'orange'},
+//       'CA': {fill: 'orange'},
+//       'NY': {fill: 'orange'}
+//       //'FL': {fill: 'light blue'}
+//     }
+//   });
 
   $('#on-ma').click(function(event){
     $('#map').usmap('trigger', 'MA', 'mouseover', event);
@@ -46,3 +205,16 @@ $(document).ready(function() {
 
 
   });
+
+
+  /*
+     <script>
+    $(document).ready(function () {
+    
+    $('#clicked-state').load('http://electrix.com/usrep/' + description + '.html');
+     $("body").css("background-color", "#333");
+  } 
+    });
+  });  
+</script>
+  */
