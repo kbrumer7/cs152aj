0cfe2c8e207a7789e734f077ab11d49038aac119
jnissenbaum01 <jnissenbaum@brandeis.edu>
2020-06-22 13:36:01 -0400

    even more Heroku

--- config/auth.js
+++ config/auth.js
@@ -5,7 +5,7 @@ module.exports = {
         'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
         'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
 
-        'callbackURL': 'https://lit-bastion-08343.herokuapp.com/'
+        'callbackURL': 'https://lit-bastion-08343.herokuapp.com/login/authorized'
 
         // 'callbackURL'   : 'http://127.0.0.1:5500/login/authorized'
         //'callbackURL': 'https://hidden-brook-99342.herokuapp.com/'
