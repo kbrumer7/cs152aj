2fb44e3dddbd9fd64d58f30fd552e1237d543243
jnissenbaum01 <jnissenbaum@brandeis.edu>
2020-06-23 15:05:39 -0400

    capitalization fix

--- routes/favs.js
+++ routes/favs.js
@@ -5,7 +5,7 @@ const express = require('express');
 const router = express.Router();
 const FavStates = require('../models/FavStates')
 
-const Forum = require('../models/forum')
+const Forum = require('../models/Forum')
 
 
 /*
